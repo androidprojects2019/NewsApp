02215b1439ded064e6d96f738d05b906f17c2346
Mohamed <mohamed.adel201568@gmail.com>
2019-10-21 01:47:18 +0200

    convert splash to mvvm pattern

--- app/src/main/java/com/example/newsapp/SplashActivity.java
+++ app/src/main/java/com/example/newsapp/SplashActivity.java
@@ -1,17 +1,20 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import com.example.newsapp.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        ActivitySplashBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
